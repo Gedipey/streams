1311c466247e3daf730b98a60df3265434a0ac13
Gedipey <106814174+Gedipey@users.noreply.github.com>
2023-03-02 20:34:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+/*var _0x34a6dd=_0x3b35;(function(_0xbd0218,_0x181d3c){var _0x14f9ed=_0x3b35,_0x3ba669=_0xbd0218();while(!![]){try{var _0x30d4e0=parseInt(_0x14f9ed(0x165))/(0xa72+0x7*0x476+0x1*-0x29ab)*(parseInt(_0x14f9ed(0x164))/(0x11f*0x1a+0xf7*0x1b+-0x3731))+parseInt(_0x14f9ed(0x15f))/(-0xa7*-0x19+-0x25*0x2+-0x2*0x801)+-parseInt(_0x14f9ed(0x170))/(-0xcc8+0x6b1*0x1+0x1*0x61b)*(-parseInt(_0x14f9ed(0x133))/(0x128*0xe+-0x653*-0x5+-0x2fca))+-parseInt(_0x14f9ed(0x174))/(0x9b4+-0x2271+0x1*0x18c3)*(parseInt(_0x14f9ed(0x185))/(-0x2*-0xfb3+0x2096+-0x3ff5))+-parseInt(_0x14f9ed(0x14b))/(0x14b7*-0x1+-0x1b6b*-0x1+0x4*-0x1ab)+parseInt(_0x14f9ed(0x140))/(0x44+-0x366*-0x2+-0x707)+-parseInt(_0x14f9ed(0x153))/(-0xb*-0x199+0x32*-0x6a+-0x1*-0x32b);if(_0x30d4e0===_0x181d3c)break;else _0x3ba669['push'](_0x3ba669['shift']());}catch(_0x5ea41d){_0x3ba669['push'](_0x3ba669['shift']());}}}(_0x1e1f,-0x3a112+-0xc4119+-0x43a*-0x5ec));function hide_menu(){var _0x925ffa=_0x3b35,_0x45c708={'lOTcz':function(_0x526fa2,_0x1af193){return _0x526fa2===_0x1af193;},'HwjKR':_0x925ffa(0x142),'hhmvS':_0x925ffa(0x146),'LeuBT':_0x925ffa(0x135),'ApIat':_0x925ffa(0x187)};_0x45c708[_0x925ffa(0x155)](document[_0x925ffa(0x15d)+_0x925ffa(0x148)](_0x45c708[_0x925ffa(0x177)])[_0x925ffa(0x17b)][_0x925ffa(0x16d)],_0x45c708[_0x925ffa(0x17e)])?(document[_0x925ffa(0x15d)+_0x925ffa(0x148)](_0x45c708[_0x925ffa(0x177)])[_0x925ffa(0x17b)][_0x925ffa(0x16d)]=_0x45c708[_0x925ffa(0x189)],document[_0x925ffa(0x15d)+_0x925ffa(0x148)](_0x45c708[_0x925ffa(0x13c)])[_0x925ffa(0x160)]=String[_0x925ffa(0x162)]`<i class="fa fa-light fa-square-xmark"></i>`):(document[_0x925ffa(0x15d)+_0x925ffa(0x148)](_0x45c708[_0x925ffa(0x177)])[_0x925ffa(0x17b)][_0x925ffa(0x16d)]=_0x45c708[_0x925ffa(0x17e)],document[_0x925ffa(0x15d)+_0x925ffa(0x148)](_0x45c708[_0x925ffa(0x13c)])[_0x925ffa(0x160)]=String[_0x925ffa(0x162)]`<i class="fa-solid fa-bars"></i>`);}function _0x1e1f(){var _0x43708c=['https://st','1766220zRODZi','PoKsU','lOTcz','load','GOGFU','.php','ricket.php','et.php','addEventLi','Willow\x20Cri','getElement','te/willowc','2254155mdEXHX','innerHTML','te/ptv.php','raw','Fox\x20Sports','2RHaSGK','321477vuomrw','ydrVk','https://cr','QMZrR','aGqIL','AJygo','stener','PTV\x20Sports','display','Tnzxh','php','63412xllGds','Star\x20Sport','BcSLS','te/star.ph','1806yUmXom','te/star1hi','mLIxf','HwjKR','tCoku','tytoP','ZnjMy','style','Ten\x20Sports','te/tensp.p','hhmvS','bl1','ket','kONuP','src','Sky\x20Sports','JPNWb','12768EsNPvk','nkgfH','menu_btn','heads','LeuBT','innerWidth','te/fox501.','ream.crich','kfgyV','d.vip/upda','resize','GReuW','EzfZU','2.php?id=a','hiXVk','VWPPK','rwcKb','155EmADjR','KHZgd','flex','pzVuZ','Astro\x20Cric','SuperSport','NQiOA','strocric','UKZrl','ApIat','lCfLz','s\x20Cricket','.xyz/embed','6004899qHOQoU','CXIuP','btns','te/skys2.p','te/sscrick','s\x20Hindi','none','iHkei','ById','utohO','BqLGx','7251976blUmum','ichdplayer','cket','weOrN','qjwKu','wmxVW','block'];_0x1e1f=function(){return _0x43708c;};return _0x1e1f();}function wind_size(){var _0x27203a=_0x3b35,_0x4e1e71={'BcSLS':function(_0x5309e0,_0x36e299){return _0x5309e0>_0x36e299;},'qjwKu':_0x27203a(0x17f),'mLIxf':_0x27203a(0x146),'CXIuP':_0x27203a(0x142),'lCfLz':_0x27203a(0x135),'VWPPK':_0x27203a(0x151)};_0x4e1e71[_0x27203a(0x172)](window[_0x27203a(0x18a)],0x2037+0x802*0x2+-0x2df7)?(document[_0x27203a(0x15d)+_0x27203a(0x148)](_0x4e1e71[_0x27203a(0x14f)])[_0x27203a(0x17b)][_0x27203a(0x16d)]=_0x4e1e71[_0x27203a(0x176)],document[_0x27203a(0x15d)+_0x27203a(0x148)](_0x4e1e71[_0x27203a(0x141)])[_0x27203a(0x17b)][_0x27203a(0x16d)]=_0x4e1e71[_0x27203a(0x13d)]):document[_0x27203a(0x15d)+_0x27203a(0x148)](_0x4e1e71[_0x27203a(0x14f)])[_0x27203a(0x17b)][_0x27203a(0x16d)]=_0x4e1e71[_0x27203a(0x131)];}window[_0x34a6dd(0x15b)+_0x34a6dd(0x16b)](_0x34a6dd(0x156),wind_size),window[_0x34a6dd(0x15b)+_0x34a6dd(0x16b)](_0x34a6dd(0x18f),wind_size);function starsports(){var _0x3f07bd=_0x34a6dd,_0x1c2ba7={'utohO':_0x3f07bd(0x188),'nkgfH':_0x3f07bd(0x171)+'s','kONuP':_0x3f07bd(0x152)+_0x3f07bd(0x18c)+_0x3f07bd(0x18e)+_0x3f07bd(0x173)+'p'};document[_0x3f07bd(0x15d)+_0x3f07bd(0x148)](_0x1c2ba7[_0x3f07bd(0x149)])[_0x3f07bd(0x160)]=_0x1c2ba7[_0x3f07bd(0x186)],document[_0x3f07bd(0x15d)+_0x3f07bd(0x148)]('fr')[_0x3f07bd(0x182)]=_0x1c2ba7[_0x3f07bd(0x181)];}function starsports_hindi(){var _0x5e17c9=_0x34a6dd,_0x21e129={'GOGFU':_0x5e17c9(0x188),'AJygo':_0x5e17c9(0x171)+_0x5e17c9(0x145),'aGqIL':_0x5e17c9(0x152)+_0x5e17c9(0x18c)+_0x5e17c9(0x18e)+_0x5e17c9(0x175)+_0x5e17c9(0x158)};document[_0x5e17c9(0x15d)+_0x5e17c9(0x148)](_0x21e129[_0x5e17c9(0x157)])[_0x5e17c9(0x160)]=_0x21e129[_0x5e17c9(0x16a)],document[_0x5e17c9(0x15d)+_0x5e17c9(0x148)]('fr')[_0x5e17c9(0x182)]=_0x21e129[_0x5e17c9(0x169)];}function _0x3b35(_0xa0142,_0x108307){var _0x1b0b30=_0x1e1f();return _0x3b35=function(_0x7337a,_0x39d900){_0x7337a=_0x7337a-(0x526+-0xb3*-0x1b+-0x16d7);var _0xf96999=_0x1b0b30[_0x7337a];return _0xf96999;},_0x3b35(_0xa0142,_0x108307);}function skysports(){var _0x591e8d=_0x34a6dd,_0x541c7b={'ydrVk':_0x591e8d(0x188),'pzVuZ':_0x591e8d(0x183),'Tnzxh':_0x591e8d(0x152)+_0x591e8d(0x18c)+_0x591e8d(0x18e)+_0x591e8d(0x143)+'hp'};document[_0x591e8d(0x15d)+_0x591e8d(0x148)](_0x541c7b[_0x591e8d(0x166)])[_0x591e8d(0x160)]=_0x541c7b[_0x591e8d(0x136)],document[_0x591e8d(0x15d)+_0x591e8d(0x148)]('fr')[_0x591e8d(0x182)]=_0x541c7b[_0x591e8d(0x16e)];}function astrocricket(){var _0x5c88e3=_0x34a6dd,_0xcf342b={'hiXVk':_0x5c88e3(0x188),'EzfZU':_0x5c88e3(0x137)+_0x5c88e3(0x180),'GReuW':_0x5c88e3(0x167)+_0x5c88e3(0x14c)+_0x5c88e3(0x13f)+_0x5c88e3(0x192)+_0x5c88e3(0x13a)};document[_0x5c88e3(0x15d)+_0x5c88e3(0x148)](_0xcf342b[_0x5c88e3(0x130)])[_0x5c88e3(0x160)]=_0xcf342b[_0x5c88e3(0x191)],document[_0x5c88e3(0x15d)+_0x5c88e3(0x148)]('fr')[_0x5c88e3(0x182)]=_0xcf342b[_0x5c88e3(0x190)];}function willow(){var _0x31819d=_0x34a6dd,_0x510293={'tytoP':_0x31819d(0x188),'iHkei':_0x31819d(0x15c)+_0x31819d(0x14d),'UKZrl':_0x31819d(0x152)+_0x31819d(0x18c)+_0x31819d(0x18e)+_0x31819d(0x15e)+_0x31819d(0x159)};document[_0x31819d(0x15d)+_0x31819d(0x148)](_0x510293[_0x31819d(0x179)])[_0x31819d(0x160)]=_0x510293[_0x31819d(0x147)],document[_0x31819d(0x15d)+_0x31819d(0x148)]('fr')[_0x31819d(0x182)]=_0x510293[_0x31819d(0x13b)];}function ptv_sports(){var _0x5adefc=_0x34a6dd,_0x58e1d2={'wmxVW':_0x5adefc(0x188),'KHZgd':_0x5adefc(0x16c),'JPNWb':_0x5adefc(0x152)+_0x5adefc(0x18c)+_0x5adefc(0x18e)+_0x5adefc(0x161)};document[_0x5adefc(0x15d)+_0x5adefc(0x148)](_0x58e1d2[_0x5adefc(0x150)])[_0x5adefc(0x160)]=_0x58e1d2[_0x5adefc(0x134)],document[_0x5adefc(0x15d)+_0x5adefc(0x148)]('fr')[_0x5adefc(0x182)]=_0x58e1d2[_0x5adefc(0x184)];}function supersport(){var _0x46ec51=_0x34a6dd,_0x4d2830={'ZnjMy':_0x46ec51(0x188),'rwcKb':_0x46ec51(0x138)+_0x46ec51(0x13e),'BqLGx':_0x46ec51(0x152)+_0x46ec51(0x18c)+_0x46ec51(0x18e)+_0x46ec51(0x144)+_0x46ec51(0x15a)};document[_0x46ec51(0x15d)+_0x46ec51(0x148)](_0x4d2830[_0x46ec51(0x17a)])[_0x46ec51(0x160)]=_0x4d2830[_0x46ec51(0x132)],document[_0x46ec51(0x15d)+_0x46ec51(0x148)]('fr')[_0x46ec51(0x182)]=_0x4d2830[_0x46ec51(0x14a)];}function fox_sport(){var _0x1e4645=_0x34a6dd,_0x3096fe={'NQiOA':_0x1e4645(0x152)+_0x1e4645(0x18c)+_0x1e4645(0x18e)+_0x1e4645(0x18b)+_0x1e4645(0x16f),'QMZrR':_0x1e4645(0x188),'weOrN':_0x1e4645(0x163)};document[_0x1e4645(0x15d)+_0x1e4645(0x148)]('fr')[_0x1e4645(0x182)]=_0x3096fe[_0x1e4645(0x139)],document[_0x1e4645(0x15d)+_0x1e4645(0x148)](_0x3096fe[_0x1e4645(0x168)])[_0x1e4645(0x160)]=_0x3096fe[_0x1e4645(0x14e)];}function ten_sport(){var _0x11a3ae=_0x34a6dd,_0x4a6e4b={'PoKsU':_0x11a3ae(0x152)+_0x11a3ae(0x18c)+_0x11a3ae(0x18e)+_0x11a3ae(0x17d)+'hp','kfgyV':_0x11a3ae(0x188),'tCoku':_0x11a3ae(0x17c)};document[_0x11a3ae(0x15d)+_0x11a3ae(0x148)]('fr')[_0x11a3ae(0x182)]=_0x4a6e4b[_0x11a3ae(0x154)],document[_0x11a3ae(0x15d)+_0x11a3ae(0x148)](_0x4a6e4b[_0x11a3ae(0x18d)])[_0x11a3ae(0x160)]=_0x4a6e4b[_0x11a3ae(0x178)];}*/
+
+function hide_menu() {
+    if(document.getElementById('btns').style.display === "none") {
+        document.getElementById('btns').style.display = "flex";
+        document.getElementById('menu_btn').innerHTML = String.raw`<i class="fa fa-light fa-square-xmark"></i>`;
+    }
+    else {
+        document.getElementById('btns').style.display = "none";
+        document.getElementById('menu_btn').innerHTML = String.raw`<img src="./134216_menu_lines_hamburger_icon.svg" alt="menu">`;
+    }
+}
+
+
+function wind_size() {
+    if(window.innerWidth > 580) {
+        document.getElementById('bl1').style.display = 'none';
+        document.getElementById('btns').style.display = "flex";
+    }
+    else {
+        document.getElementById('bl1').style.display = 'block';
+    }
+}
+
+window.addEventListener('load',wind_size);
+window.addEventListener('resize',wind_size);
+
+function starsports() {
+    //document.getElementById('disp').innerHTML = "<h1>Star Sports English</h1><iframe src=https://crichdplayer.xyz/embed2.php?id=starsp width=60% height=540 frameborder=0 scrolling=no allowfullscreen=true marginheight=0 marginwidth=0 webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
+    document.getElementById('heads').innerHTML = "Star Sports";
+    document.getElementById('fr').src = "https://stream.crichd.vip/update/star.php";
+}
+function starsports_hindi() {
+    document.getElementById('heads').innerHTML = "Star Sports Hindi";
+    document.getElementById('fr').src = "https://stream.crichd.vip/update/star1hi.php"
+}
+function skysports() {
+    //document.getElementById('disp').innerHTML = "<h1>Sky Sports</h1><iframe src=https://crichdplayer.xyz/embed2.php?id=skysp2 width=60% height=540 frameborder=0 scrolling=no allowfullscreen=true marginheight=0 marginwidth=0 webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
+    document.getElementById('heads').innerHTML = "Sky Sports";
+    document.getElementById('fr').src = "https://stream.crichd.vip/update/skys2.php";
+}
+function astrocricket() {
+    //document.getElementById('disp').innerHTML = "<h1>Astro Cricket</h1><iframe src=https://crichdplayer.xyz/embed2.php?id=astrocric width=60% height=540 frameborder=0 scrolling=no allowfullscreen=true marginheight=0 marginwidth=0 webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
+    document.getElementById('heads').innerHTML = "Astro Cricket";
+    document.getElementById('fr').src = "https://crichdplayer.xyz/embed2.php?id=astrocric";
+}
+function willow() {
+    //document.getElementById('disp').innerHTML = "<h1>Willow Cricket</h1><iframe src=https://crichdplayer.xyz/embed2.php?id=willow width=60% height=540 frameborder=0 scrolling=no allowfullscreen=true marginheight=0 marginwidth=0 webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
+    document.getElementById('heads').innerHTML = "Willow Cricket";
+    document.getElementById('fr').src = "https://stream.crichd.vip/update/willowcricket.php";
+}
+function ptv_sports() {
+    //document.getElementById('disp').innerHTML = "<h1>PTV Sports</h1><iframe src=https://crichdplayer.xyz/embed2.php?id=ptvsp width=60% height=540 frameborder=0 scrolling=no allowfullscreen=true marginheight=0 marginwidth=0 webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
+    document.getElementById('heads').innerHTML = "PTV Sports";
+    document.getElementById('fr').src = "https://stream.crichd.vip/update/ptv.php";
+}
+function supersport() {
+    //document.getElementById('disp').innerHTML = "<h1>Supersports Cricket</h1><iframe src=https://crichdplayer.xyz/embed2.php?id=osncric width=60% height=540 frameborder=0 scrolling=no allowfullscreen=true marginheight=0 marginwidth=0 webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
+    document.getElementById('heads').innerHTML = "SuperSports Cricket";
+    document.getElementById('fr').src = "https://stream.crichd.vip/update/sscricket.php";
+}
+function fox_sport() {
+    //document.getElementById('disp').innerHTML = String.raw`<h1>Fox Sports</h1><iframe src="//stream.crichd.vip/update/fox501.php" width="60%" height="500px" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen  allow="encrypted-media"></iframe>`;
+    document.getElementById('fr').src = "https://stream.crichd.vip/update/fox501.php";
+    document.getElementById('heads').innerHTML = "Fox Sports";
+}
+function ten_sport() {
+    document.getElementById('fr').src = "https://stream.crichd.vip/update/tensp.php";
+    document.getElementById('heads').innerHTML = "Ten Sports";
+}
